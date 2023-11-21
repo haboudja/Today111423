5afb11864ea7236856033d63f6c0de6ac4012c42
Hyacinthe Aboudja <haboudja@okcu.edu>
2023-11-21 14:20:13 -0600

    comminting CalculatorTemplate to MyTests

---
+++ Calculatortemplate.java
@@ -0,0 +1,32 @@
+public class Calculator {
+    private int result;
+
+    public Calculator() {
+        result = 0;
+    }
+
+   // add
+
+    // substract
+
+    // multiply
+
+// divide
+    public int getResult() {
+        return result;
+    }
+
+    public static void main(String[] args) {
+//declare 2 numbers
+
+	int num1 = 24, num2 = 38;
+// instantiate an object calculator
+
+// add num1
+        
+// substract num2
+
+//multiply num1 and num2
+// display the result
+    }
+}
